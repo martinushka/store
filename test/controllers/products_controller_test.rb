fe28df9aadb45b859303f4974021a483a4e41b16
martinushka <martinushka536@yandex.ru>
2022-04-30 00:26:42 +0300

    Validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,11 +17,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: {
+        product: {
+          description: @product.description,
+          image_url: @product.image_url,
+          price: @product.price,
+          title: @title,
+        }
+      }
     end
-
     assert_redirected_to product_url(Product.last)
   end
+
 
   test "should show product" do
     get product_url(@product)
@@ -34,9 +41,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: {
+      product: {
+        description: @product.description,
+        image_url: @product.image_url,
+        price: @product.price,
+        title: @title,
+      }
+    }
     assert_redirected_to product_url(@product)
   end
+
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
