78e2fb3a13b709be91b78ae71c5381b039b7eef8
martinushka <martinushka536@yandex.ru>
2022-05-03 15:19:08 +0300

    adding sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders
+  resources :line_items
+  resources :carts
+  resources :products do
+    get :who_bought, on: :member
+  end
+  root to: 'store#index', as: 'store_index'
+  get 'store/index'
+  # For details on the DSL available within this file, see
+  # https://guides.rubyonrails.org/routing.html
 end
